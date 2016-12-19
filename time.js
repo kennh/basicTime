e4ebe7dc396217db887064807d20b27a36552ccb
Kennon Houck <kennon_h@hotmail.com>
2016-12-19 13:40:51 -0800

    initial commit of first project in javascript

---
+++ time.js
@@ -0,0 +1,15 @@
+var hour = 8;
+var minute= 50;
+var period = "am";
+function detTime(hour, minute, period){
+  if (period== "am"){
+    var postTime = " in the morning";
+  } else {
+    var postTime = " in the evening";
+  }
+switch (1){
+  case minute < 30: return "just after "+hour + postTime;
+    default: return "almost " + (hour+ 1) + postTime;
+}
+}
+console.log(detTime(8,25,"am"));
